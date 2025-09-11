d7af027acd9db8cdc0a443b5e0786135dd8bf55c
ViniciusBaylet <viniciusbaylet6@gmail.com>
2025-09-11 11:57:53 -0300

    configurando Stitches

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { getCssText } from "../styles";
 import "./globals.css";
 import { Roboto } from "next/font/google"
 
@@ -6,6 +7,9 @@ const roboto = Roboto({ weight: ["400", "700"] });
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="pt-br">
+      <head>
+        <style id="stitches" dangerouslySetInnerHTML={{__html: getCssText() }} />
+      </head>
       <body className={roboto.className}>
         {children}
       </body>
